7c296e639ca0d7d7710a2cca466cbdacb02a8bfa
Styles <stylesk11@gmail.com>
2024-07-02 13:58:30 -0700

    add interface

--- components/project.tsx
+++ components/project.tsx
@@ -1,5 +1,12 @@
+interface ProjectProps {
+  project: {
+    name: string;
+    description: string;
+    images: {}
+  }
+}
 
-const Project = ({ project }) => {
+const Project = ({ project }: ProjectProps) => {
   return (
     <div>
       <h1>{project.name}</h1>
