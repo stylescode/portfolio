19709391a7183dfa33868423e8377a1913395f98
Styles <stylesk11@gmail.com>
2024-07-06 13:51:54 -0700

    import components

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,31 @@
+import About from '@/components/about';
+import Connect from '@/components/connect';
+import Projects from '@/components/projects';
+import Descriptor from '@/components/descriptor';
+import Tech from '@/components/tech';
+
+import myprojects from '@/data/projects.json';
 
 export default function Home() {
+
+  const skills = ["css3", "express", "git", "html5", "javascript", "jest", "jquery", "mongodb", "mysql", "nginx", "nextjs", "nodejs", "npm", "postgresql", "postman", "prisma", "python", "react", "ruby", "supabase", "tailwindcss", "typescript", "vercel", "vite", "vscode"];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <div>
-      Portfolio site in progress
-     </div>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <div className="flex">
+        <p>About</p>
+        <p>Skills</p>
+        <p>Projects</p>
+        <p>Connect</p>
+      </div>
+      <div className="flex flex-col items-center">
+        <h1>STYLES KIM</h1>
+        <Descriptor />
+      </div>
+      <About />
+      <Tech technologies={skills} />
+      <Projects projects={myprojects} />
+      <Connect />
     </main>
   );
 }
