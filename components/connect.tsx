5317670ec63179678d5df2740facaa74ad310b86
Styles <stylesk11@gmail.com>
2024-07-08 14:01:38 -0700

    add section for connecting w/ me

---
+++ components/connect.tsx
@@ -0,0 +1,21 @@
+
+const Connect = () => {
+
+  return (
+    <div>
+      <div>
+        Connect with me:
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/styles-kim/">
+          LinkedIn
+        </a>
+        <a href="https://github.com/stylescode">
+          GitHub
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Connect;
