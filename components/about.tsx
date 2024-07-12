3cf5f10291bc3923399d1c5b0f5568685fbb828c
Styles <stylesk11@gmail.com>
2024-07-11 22:04:29 -0700

    add about section

---
+++ components/about.tsx
@@ -0,0 +1,11 @@
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Welcome to my corner of the web. I am a full-stack software engineer based in SoCal, committed to crafting user-friendly & innovative apps through meticulous problem solving and creative thinking. I thrive on the endless well of knowledge this world provides with a strong dedication toward continuous learning. I aim to deliver impactful, high-quality results in dynamic and collaborative work spaces with a keen awareness of the evolving tech landscape, sturdy foundational skills, and an understanding of communication's pivotal role in achieving success.</p>
+    </div>
+  )
+}
+
+export default About;
