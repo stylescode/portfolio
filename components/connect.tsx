6633fb40fadaaa4106f7b0c2a47c6b7911ef7230
Styles <stylesk11@gmail.com>
2024-07-19 15:48:49 -0700

    update image sources

--- components/connect.tsx
+++ components/connect.tsx
@@ -1,17 +1,18 @@
+import Image from 'next/image';
 
 const Connect = () => {
 
   return (
-    <div>
+    <div className="bg-gray-700 w-full flex flex-col items-center py-[40px] text-[30px]">
       <div>
-        Connect with me:
+        Connect w/ Me:
       </div>
-      <div>
+      <div className="flex">
         <a href="https://www.linkedin.com/in/styles-kim/">
-          LinkedIn
+          <Image width={200} height={200} src='https://upload.wikimedia.org/wikipedia/commons/1/19/LinkedIn_logo.svg' alt='LinkedIn logo' />
         </a>
         <a href="https://github.com/stylescode">
-          GitHub
+          <Image width={200} height={200} src='https://pngimg.com/uploads/github/github_PNG23.png' alt='GitHub logo'/>
         </a>
       </div>
     </div>
