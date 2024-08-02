d21e8d03034dd2f8b387aa56fdcaddba3afd5e9d
Styles <stylesk11@gmail.com>
2024-08-02 13:17:43 -0700

    update layout of projects

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,7 +3,7 @@ import Project from './project';
 
 const Projects = ({ projects }) => {
   return (
-    <div>
+    <div className="bg-gray-600 max-w-full px-[40px] py-[40px] grid ">
       {projects.map((project) => (
         <Project key={project.name} project={project} />
       ))}
