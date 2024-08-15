3fbfc5a9e4ee361b914244bf61d43289c3997334
Styles <stylesk11@gmail.com>
2024-08-15 13:07:16 -0700

    display tech using icon slider

--- components/tech.tsx
+++ components/tech.tsx
@@ -1,19 +1,42 @@
 import Image from 'next/image';
+import IconSlider from './IconSlider';
 
 const Tech = ({}) => {
 
-  const skills = [["css", "CSS"], ["express", "Express.js"], ["git", "Git"], ["html", "HTML"], ["javascript", "JavaScript"], ["jest", "Jest"], ["jquery", "jQuery"], ["mongodb", "MongoDB"], ["mysql", "MySQL"], ["nginx", "NGINX"], ["nextjs", "Next.js"], ["nodejs", "Node.js"], ["npm", "NPM"], ["postgresql", "PostgreSQL"], ["postman", "Postman"], ["prisma", "Prisma"], ["python", "Python"], ["react", "React"], ["ruby", "Ruby"], ["supabase", "Supabase"], ["tailwindcss", "Tailwind"], ["typescript", "TypeScript"], ["vercel", "Vercel"], ["vite", "Vite"], ["vscode", "VS Code"]];
+  const skills = [
+    { name: "css", image: "CSS" },
+    { name: "express", image: "Express.js" },
+    { name: "git", image: "Git" },
+    { name: "html", image: "HTML" },
+    { name: "javascript", image: "JavaScript" },
+    { name: "jest", image: "Jest" },
+    { name: "jquery", image: "jQuery" },
+    { name: "mongodb", image: "MongoDB" },
+    { name: "mysql", image: "MySQL" },
+    { name: "nginx", image: "NGINX" },
+    { name: "nextjs", image: "Next.js" },
+    { name: "nodejs", image: "Node.js" },
+    { name: "npm", image: "NPM" },
+    { name: "postgresql", image: "PostgreSQL" },
+    { name: "postman", image: "Postman" },
+    { name: "prisma", image: "Prisma" },
+    { name: "python", image: "Python" },
+    { name: "react", image: "React" },
+    { name: "ruby", image: "Ruby" },
+    { name: "supabase", image: "Supabase" },
+    { name: "tailwindcss", image: "Tailwind" },
+    { name: "typescript", image: "TypeScript" },
+    { name: "vercel", image: "Vercel" },
+    { name: "vite", image: "Vite" },
+    { name: "vscode", image: "VS Code" }
+  ];
 
   return (
     <div className="flex w-full bg-gray-700 overflow-hidden">
-      {skills.map((tech) => (
-        <div key={tech[0]} className="w-[200px] h-[200px] flex flex-col items-center">
-          <Image src={`https://skillicons.dev/icons?i=${tech[0]}`} width={60} height={60} alt={`${tech[0]} icon`}/>
-          <div>{tech[1]}</div>
-        </div>
-      ))}
+      <IconSlider icons={skills} />
     </div>
   );
 }
 
 export default Tech;
+
