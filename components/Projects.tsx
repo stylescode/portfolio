407c995bbef945a883a73d722e9b711264deb80a
Styles <stylesk11@gmail.com>
2025-07-08 14:59:42 -0700

    render all projects in dedicated section

---
+++ components/Projects.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Project from './Project';
+
+interface ProjectType {
+  name: string;
+  description: string;
+  images: string[];
+  demoLink: string;
+  codeLink: string;
+}
+
+interface ProjectsProps {
+  projects: ProjectType[];
+}
+
+const Projects = ({ projects }: ProjectsProps) => {
+  return (
+    <div className="border border-black flex w-3/4" >
+      {projects.map((project) => (
+        <Project key={project.name} project={project} />
+      ))}
+    </div>
+  );
+}
+
+export default Projects;
